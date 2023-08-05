d8abedbb633a85b77fcbf94108bf8dc305b6c5c5
juniordv23 <141356409+juniordv23@users.noreply.github.com>
2023-08-05 18:47:14 -0500

    Update link.php

--- metronic/include/link.php
+++ metronic/include/link.php
@@ -1,44 +1,17 @@
 <?php
 
-/*$servername = "http://3.143.219.233/";
-$database = "metr0n1c";
-$username = "freedb_NACIONAL";
-$password = "%8HeUhRP%aSkr?F";
-$port = "3306";
 
-$servername = "3.143.219.233";
-$database = "metr0n1c";
-$username = "junior";
-$password = "junior";
-
-
-
-
-
-
-
-
-
-
-$servername = "containers-us-west-41.railway.app";
-$database = "railway";
-$username = "root";
-$password = "1mp1y4fs616OUFtKS5sK";
-*/
-
-
-
-$servername = $_ENV["DB_HOST"];
-$database =  $_ENV["DB_NAME"];
-$username = $_ENV["DB_USER"];
-$password = $_ENV["DB_PASSWORD"];
+$DB_HOST = $_ENV["DB_HOST"];
+$DB_NAME =  $_ENV["DB_NAME"];
+$DB_USER = $_ENV["DB_USER"];
+$DB_PASSWORD = $_ENV["DB_PASSWORD"];
 $DB_PORT = $_ENV["DB_PORT"];
 
 
 function conectar (){
 	//$conn = mysqli_connect($GLOBALS["servername"], $GLOBALS["username"], $GLOBALS["password"], $GLOBALS["database"]), $GLOBALS["port"]);	
 	// $conn = mysqli_connect($GLOBALS["servername"], $GLOBALS["username"], $GLOBALS["password"], $GLOBALS["database"]);	
-	$conn = mysqli_connect("$DB_HOST","$DB_USER","$DB_PASSWORD", "$DB_NAME");	
+	$conn = mysqli_connect("$DB_HOST","$DB_USER","$DB_PASSWORD", "$DB_NAME", "$DB_PORT");	
 	
 	if (!$conn) {
 	    die("Connection failed: " . mysqli_connect_error());
